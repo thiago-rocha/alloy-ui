df2c74e82916443db565173c2f279a39e7129159
Thiago Rocha <thiago.rocha@liferay.com>
2015-10-29 17:40:06 -0300

    AUI-2035 Source formatting

--- src/aui-button/js/aui-button-core.js
+++ src/aui-button/js/aui-button-core.js
@@ -4,24 +4,15 @@
  * @module aui-button
  */
 
-var Lang = A.Lang,
-    isArray = Lang.isArray,
-    isBoolean = Lang.isBoolean,
-    isNumber = Lang.isNumber,
-    isString = Lang.isString,
-    isUndefined = Lang.isUndefined,
-
-    getClassName = A.getClassName,
-
-    CLASS_NAMES = {
-        BUTTON: getClassName('btn'),
-        BUTTON_DEFAULT: getClassName('btn', 'default'),
-        BUTTON_GROUP: getClassName('btn', 'group'),
-        DISABLED: getClassName('disabled'),
-        LABEL: getClassName('label'),
-        PRIMARY: getClassName('btn', 'primary'),
-        SELECTED: getClassName('active'),
-        TOGGLE: getClassName('togglebtn')
+var CLASS_NAMES = {
+        BUTTON: A.getClassName('btn'),
+        BUTTON_DEFAULT: A.getClassName('btn', 'default'),
+        BUTTON_GROUP: A.getClassName('btn', 'group'),
+        DISABLED: A.getClassName('disabled'),
+        LABEL: A.getClassName('label'),
+        PRIMARY: A.getClassName('btn', 'primary'),
+        SELECTED: A.getClassName('active'),
+        TOGGLE: A.getClassName('togglebtn')
     };
 
 /**
@@ -33,9 +24,7 @@ var Lang = A.Lang,
  * @constructor
  */
 var ButtonExt = function(config) {
-    var instance = this;
-
-    instance._setEarlyButtonDomType(config.domType);
+    this._setEarlyButtonDomType(config.domType);
 };
 
 /**
@@ -54,7 +43,7 @@ ButtonExt.ATTRS = {
      * @type {String}
      */
     cssClass: {
-        validator: isString,
+        validator: A.Lang.isString,
         value: ''
     },
 
@@ -66,7 +55,7 @@ ButtonExt.ATTRS = {
      * @type {Boolean}
      */
     discardDefaultButtonCssClasses: {
-        validator: isBoolean,
+        validator: A.Lang.isBoolean,
         value: false,
         writeOnce: true
     },
@@ -79,10 +68,10 @@ ButtonExt.ATTRS = {
      * @writeOnce
      */
     domType: {
-        writeOnce: true,
         validator: function(val) {
             return val.toLowerCase() === 'button' || val.toLowerCase() === 'submit';
-        }
+        },
+        writeOnce: true
     },
 
     /**
@@ -103,8 +92,7 @@ ButtonExt.ATTRS = {
      */
     iconElement: {
         valueFn: function() {
-            var instance = this;
-            return A.Node.create(instance.ICON_TEMPLATE);
+            return A.Node.create(this.ICON_TEMPLATE);
         }
     },
 
@@ -116,7 +104,7 @@ ButtonExt.ATTRS = {
      * @type {String}
      */
     iconAlign: {
-        validator: isString,
+        validator: A.Lang.isString,
         value: 'left'
     }
 };
@@ -145,7 +133,7 @@ ButtonExt.HTML_PARSER = {
  * @static
  */
 ButtonExt.getTypedButtonTemplate = function(template, type) {
-    return Lang.sub(template, {
+    return A.Lang.sub(template, {
         type: type ? ' type="' + type + '"' : ''
     });
 };
@@ -161,13 +149,11 @@ ButtonExt.prototype = {
      * @protected
      */
     initializer: function() {
-        var instance = this;
-
-        instance.before(instance.renderButtonExtUI, instance, 'renderUI');
-        instance.after(instance.syncButtonExtUI, instance, 'syncUI');
-        instance.after({
-            iconChange: instance._afterIconChange,
-            iconAlignChange: instance._afterIconAlignChange
+        this.before(this.renderButtonExtUI, this, 'renderUI');
+        this.after(this.syncButtonExtUI, this, 'syncUI');
+        this.after({
+            iconChange: this._afterIconChange,
+            iconAlignChange: this._afterIconAlignChange
         });
     },
 
@@ -179,12 +165,11 @@ ButtonExt.prototype = {
      * @protected
      */
     renderButtonExtUI: function() {
-        var instance = this,
-            cssClass = instance.get('cssClass');
+        var cssClass = this.get('cssClass');
 
-        if (!instance.get('discardDefaultButtonCssClasses')) {
+        if (!this.get('discardDefaultButtonCssClasses')) {
             cssClass = [cssClass, CLASS_NAMES.BUTTON_DEFAULT, CLASS_NAMES.BUTTON].join(' ');
-            instance.set('cssClass', cssClass);
+            this.set('cssClass', cssClass);
         }
     },
 
@@ -194,11 +179,8 @@ ButtonExt.prototype = {
      * @method syncButtonExtUI
      */
     syncButtonExtUI: function() {
-        var instance = this;
-
-        instance._uiSetIcon(instance.get('icon'));
-
-        instance._setButtonRole();
+        this._uiSetIcon(this.get('icon'));
+        this._setButtonRole();
     },
 
     /**
@@ -209,9 +191,7 @@ ButtonExt.prototype = {
      * @protected
      */
     _afterIconChange: function(event) {
-        var instance = this;
-
-        instance._uiSetIcon(event.newVal);
+        this._uiSetIcon(event.newVal);
     },
 
     /**
@@ -222,9 +202,7 @@ ButtonExt.prototype = {
      * @protected
      */
     _afterIconAlignChange: function(event) {
-        var instance = this;
-
-        instance._uiSetIconAlign(event.newVal);
+        this._uiSetIconAlign(event.newVal);
     },
 
     /**
@@ -233,10 +211,7 @@ ButtonExt.prototype = {
      * @method setButtonRole
      */
     _setButtonRole: function() {
-        var instance = this;
-        var boundingBox = instance.get('boundingBox');
-
-        boundingBox.setAttribute('role', 'button');
+        this.get('boundingBox').setAttribute('role', 'button');
     },
 
     /**
@@ -248,9 +223,7 @@ ButtonExt.prototype = {
      * @protected
      */
     _setEarlyButtonDomType: function(type) {
-        var instance = this;
-
-        instance.BOUNDING_TEMPLATE = A.ButtonExt.getTypedButtonTemplate(
+        this.BOUNDING_TEMPLATE = A.ButtonExt.getTypedButtonTemplate(
             ButtonExt.prototype.TEMPLATE, type);
     },
 
@@ -262,14 +235,16 @@ ButtonExt.prototype = {
      * @protected
      */
     _uiSetIcon: function(val) {
-        var instance = this;
+        var iconElement;
 
         if (!val) {
             return;
         }
-        var iconElement = instance.get('iconElement');
+
+        iconElement = this.get('iconElement');
+
         iconElement.set('className', val);
-        instance._uiSetIconAlign(instance.get('iconAlign'));
+        this._uiSetIconAlign(this.get('iconAlign'));
     },
 
     /**
@@ -446,8 +421,7 @@ A.mix(ButtonGroup.prototype, {
      * @return {Button | Node} The item as `Button` or `Node` instance.
      */
     item: function(index) {
-        var instance = this,
-            node = instance.getButtons().item(index),
+        var node = this.getButtons().item(index),
             item = A.Widget.getByNode(node);
 
         if (A.instanceOf(item, Button)) {
@@ -464,11 +438,10 @@ A.mix(ButtonGroup.prototype, {
      * @protected
      */
     renderUI: function() {
-        var instance = this,
-            boundingBox = instance.get('boundingBox'),
-            type = instance.get('type');
+        var boundingBox = this.get('boundingBox'),
+            type = this.get('type');
 
-        instance.getButtons().each(function(button) {
+        this.getButtons().each(function(button) {
             if (!button.button && !A.instanceOf(A.Widget.getByNode(button), A.Button)) {
                 // TODO: This shouldn't assume button is always default.
                 // A.Plugin.Button doesn't current allow augmentation, therefore
@@ -491,7 +464,7 @@ A.mix(ButtonGroup.prototype, {
             role: 'listbox'
         });
 
-        instance.syncAriaSelected(instance.getButtons());
+        this.syncAriaSelected(this.getButtons());
     },
 
     /**
@@ -501,9 +474,7 @@ A.mix(ButtonGroup.prototype, {
      * @param {Array} items
      */
     select: function(items) {
-        var instance = this;
-
-        return instance.toggleSelect(items, true);
+        return this.toggleSelect(items, true);
     },
 
     /**
@@ -531,18 +502,18 @@ A.mix(ButtonGroup.prototype, {
      */
     toggleSelect: function(items, forceSelection) {
         var instance = this,
-            type = instance.get('type'),
-            buttons = instance.getButtons();
+            type = this.get('type'),
+            buttons = this.getButtons();
 
-        if (isUndefined(items)) {
+        if (A.Lang.isUndefined(items)) {
             items = buttons.getDOMNodes();
         }
-        if (!isArray(items)) {
+        if (!A.Lang.isArray(items)) {
             items = A.Array(items);
         }
 
         A.Array.each(items, function(item) {
-            if (isNumber(item)) {
+            if (A.Lang.isNumber(item)) {
                 item = buttons.item(item);
             }
             // Make sure the passed dom nodes are instance of Node
@@ -576,9 +547,7 @@ A.mix(ButtonGroup.prototype, {
      * @param {Array} items
      */
     unselect: function(items) {
-        var instance = this;
-
-        return instance.toggleSelect(items, false);
+        return this.toggleSelect(items, false);
     },
 
     /**
@@ -588,8 +557,6 @@ A.mix(ButtonGroup.prototype, {
      * @protected
      */
     _afterSelectionChange: function() {
-        var instance = this;
-
-        instance.syncAriaSelected(instance.getButtons());
+        this.syncAriaSelected(this.getButtons());
     }
 }, true);
