c1baf89dcb073aa6c9905de69743055e4b60e93a
Byran Zaugg <byran.zaugg@liferay.com>
2015-11-02 17:26:03 -0800

    Update A.FormValidator docs.

--- src/aui-form-validator/js/aui-form-validator.js
+++ src/aui-form-validator/js/aui-form-validator.js
@@ -583,7 +583,7 @@ var FormValidator = A.Component.create({
          * Adds a validation error in the field.
          *
          * @method addFieldError
-         * @param field
+         * @param {Node} field
          * @param ruleName
          */
         addFieldError: function(field, ruleName) {
@@ -599,10 +599,10 @@ var FormValidator = A.Component.create({
         },
 
         /**
-         * Removes a validation error in the field.
+         * Deletes the field from the errors property object.
          *
          * @method clearFieldError
-         * @param field
+         * @param {Node} field
          */
         clearFieldError: function(field) {
             var instance = this;
@@ -633,7 +633,7 @@ var FormValidator = A.Component.create({
          * Gets the ancestor of a given field.
          *
          * @method findFieldContainer
-         * @param field
+         * @param {Node} field
          * @return {Node}
          */
         findFieldContainer: function(field) {
@@ -670,7 +670,7 @@ var FormValidator = A.Component.create({
          * Gets a field from the form.
          *
          * @method getField
-         * @param fieldOrFieldName
+         * @param {Node|String} fieldOrFieldName
          * @return {Node}
          */
         getField: function(fieldOrFieldName) {
@@ -705,7 +705,7 @@ var FormValidator = A.Component.create({
          * Gets a list of fields with errors.
          *
          * @method getFieldError
-         * @param field
+         * @param {Node} field
          * @return {String}
          */
         getFieldError: function(field) {
@@ -718,7 +718,8 @@ var FormValidator = A.Component.create({
          * Gets the stack error container of a field.
          *
          * @method getFieldStackErrorContainer
-         * @param field
+         * @param {Node} field
+         * @return {Node}
          */
         getFieldStackErrorContainer: function(field) {
             var instance = this,
@@ -736,7 +737,7 @@ var FormValidator = A.Component.create({
          * Gets the error message of a field.
          *
          * @method getFieldErrorMessage
-         * @param field
+         * @param {Node} field
          * @param rule
          * @return {String}
          */
@@ -785,7 +786,7 @@ var FormValidator = A.Component.create({
          * Highlights a field with error or success.
          *
          * @method highlight
-         * @param field
+         * @param {Node} field
          * @param valid
          */
         highlight: function(field, valid) {
@@ -823,7 +824,7 @@ var FormValidator = A.Component.create({
          * Removes the highlight of a field.
          *
          * @method unhighlight
-         * @param field
+         * @param {Node} field
          */
         unhighlight: function(field) {
             var instance = this;
@@ -835,9 +836,9 @@ var FormValidator = A.Component.create({
          * Prints the stack error messages into a container.
          *
          * @method printStackError
-         * @param field
-         * @param container
-         * @param errors
+         * @param {Node} field
+         * @param {Node} container
+         * @param {Array} errors
          */
         printStackError: function(field, container, errors) {
             var instance = this;
@@ -879,10 +880,10 @@ var FormValidator = A.Component.create({
         },
 
         /**
-         * Resets the CSS class and content of a field.
+         * Resets the CSS class and error status of a field.
          *
          * @method resetField
-         * @param field
+         * @param {Node} field
          */
         resetField: function(field) {
             var instance = this,
@@ -897,7 +898,7 @@ var FormValidator = A.Component.create({
          * Removes the CSS classes of a field.
          *
          * @method resetFieldCss
-         * @param field
+         * @param {Node} field
          */
         resetFieldCss: function(field) {
             var instance = this,
@@ -921,7 +922,7 @@ var FormValidator = A.Component.create({
          * Checks if a field can be validated or not.
          *
          * @method validatable
-         * @param field
+         * @param {Node} field
          * @return {Boolean}
          */
         validatable: function(field) {
@@ -960,7 +961,7 @@ var FormValidator = A.Component.create({
          * Validates a single field.
          *
          * @method validateField
-         * @param field
+         * @param {Node|String} field
          */
         validateField: function(field) {
             var instance = this,
@@ -1132,7 +1133,7 @@ var FormValidator = A.Component.create({
          * Finds the label text of a field if existing.
          *
          * @method _findFieldLabel
-         * @param field
+         * @param {Node} field
          * @return {String}
          */
         _findFieldLabel: function(field) {
@@ -1158,10 +1159,10 @@ var FormValidator = A.Component.create({
          * field.
          *
          * @method _highlightHelper
-         * @param field
-         * @param errorClass
-         * @param validClass
-         * @param valid
+         * @param {Node} field
+         * @param {String} errorClass
+         * @param {String} validClass
+         * @param {Boolean} valid
          * @protected
          */
         _highlightHelper: function(field, errorClass, validClass, valid) {
